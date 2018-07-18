66a02003526a211262c4916dfee3d9d5eef87a00
janeduncan <janeduncan@live.co.uk>
2018-07-18 14:43:25 +0100

    adds sum array function

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,15 @@
 def add_array_lengths(array_1, array_2)
   return array_1.length() + array_2.length()
 end
+
+# -- 2 --
+
+def sum_array(array)
+  total = 0
+
+  for num in array
+    total += num
+  end
+
+  return total 
+end
