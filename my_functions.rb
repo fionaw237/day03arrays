6bd8a1179ed27e41c40b59fd0507866e3825e37f
janeduncan <janeduncan@live.co.uk>
2018-07-18 14:48:14 +0100

    adds find_item function

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,5 @@
+
+
 def add_array_lengths(array_1, array_2)
   return array_1.length() + array_2.length()
 end
@@ -11,5 +13,16 @@ def sum_array(array)
     total += num
   end
 
-  return total 
+  return total
+end
+
+# -- 3 --
+
+def find_item(houses, house_name)
+  for house in houses
+    if (house == house_name)
+      return true
+    end
+  end
+  return false
 end
